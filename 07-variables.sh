5aef77b8b31863bb1126e805b14aff690c9da7a7
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 18:37:28 +0530

    added file

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "please enter the username"
-read USERNAME
+read -s USERNAME
 echo "username entered is :$USERNAME"
+
+echo "please enter the password"
+read -s PASSWORD
+echo "password entered is :$PASSWORD"

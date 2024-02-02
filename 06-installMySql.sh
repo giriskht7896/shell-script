9a1d66793df6c59b9d0d590dc21b57f33b9a6c48
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 10:35:21 +0530

    added mysql  file

---
+++ 06-installMySql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#check root user or not
+#if not root user exit the program and inform user to run with sudo access
+#if root user install mysql
+
+USERID=$(id -u)
+if [ $USERID -ne 0]
+then
+    echo "ERROR:: please run this with root access"
+fi
+
+yum install mysql -y

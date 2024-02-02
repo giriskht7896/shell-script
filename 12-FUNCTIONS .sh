1a03c278a5874b0d030540f0581ebb0cfc319bfc
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 21:44:24 +0530

    added file

---
+++ 12-FUNCTIONS .sh
@@ -0,0 +1,25 @@
+#$/bin/bash
+
+USERID=$(id -u)
+if[ $USERID -ne 0 ]
+then 
+    echo "run this with root user "
+    exit 1
+if
+# this function validate the previous command success or failure
+VALIDATE(){
+
+    if [ $? -ne 0 ]
+    then
+        echo "failure"
+        exit 1
+    else
+        echo "success" 
+    fi
+
+}
+
+yum install nginx -y
+VALIDATE $? "installed nginx"
+yum install postfix -y
+VALIDATE $? "installed npostfix"

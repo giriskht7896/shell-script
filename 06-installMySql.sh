016be7e6821a1bdcd645406df2c6dbf815137cb7
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 20:53:46 +0530

    added file

--- 06-installMySql.sh
+++ 06-installMySql.sh
@@ -8,15 +8,8 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: please run this with root access"
-    exit 1
+  
 fi
 
 yum install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo"installation of my sql is error"
-    exit 1
-else
-    echo "installation of my sql is success"
-fi

5650ef0f4712b3ff06b8671b5ba2e51b24ee02f1
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 21:02:22 +0530

    added file

---
+++ 09-exit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check root user or not
+#if not root user exit the program and inform user to run with sudo access
+#if root user install mysql
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: please run this with root access"
+    exit 
+  
+fi
+yum install mysql -y
+

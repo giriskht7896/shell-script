854a7325211e45f459c741548130ce327eec2de3
Gireesh <gireesh@DevOps@gmail.com>
2024-02-02 21:15:26 +0530

    added file

---
+++ 10.SHELL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#check root user or not
+#if not root user exit the program and inform user to run with sudo access
+#if root user install mysql
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: please run this with root access"
+    exit 1
+  
+fi
+yum install nginx -y
+if [ $? -ne 0 ]
+then
+    echo "installation of nginx is error"
+    exit 1
+else
+    echo"installation of nginx is success" 
+fi

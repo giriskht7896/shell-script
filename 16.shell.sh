a4e300bc0eb65192d3a83ecd29250061d5729963
Gireesh <gireesh@DevOps@gmail.com>
2024-02-03 13:14:11 +0530

    added file

--- 16.shell.sh
+++ 16.shell.sh
@@ -1,10 +1,25 @@
 #$/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/home/ec2-user/shell-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[31m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " installing $R failure $2 $N"
+    else
+        echo -e " installing $G success $2 $N"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo -e "$R Error: run the command with root or sudo"
@@ -13,5 +28,14 @@ fi
 
 for i in $@
 do
-    yum install $i -y
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i already installed $N "
+    fi
+
 done

6bf0225ba8a7311ca9096e36be98692efd1f26ed
Gireesh <gireesh@DevOps@gmail.com>
2024-02-03 11:11:54 +0530

    added file

---
+++ 16.shell.sh
@@ -0,0 +1,18 @@
+#$/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: run this command with root access"
+    exit 1
+if
+
+for i in $@
+do
+    yum install $i -y
+done
